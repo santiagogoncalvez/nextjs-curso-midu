be6e0e65263eb44eb33141e3cd1681f9ec44ab74
santiagogoncalvez <santiago.goncalvez.l@gmail.com>
2026-05-28 13:59:39 -0300

    feat(customers): crear página de clientes con skeleton de carga personalizado - Se añade la ruta y vista para la página de `/dashboard/customers`. - Se implementa el componente de Skeleton específico para la tabla de clientes. - Se replica la estructura de diseño y manejo de estados asíncronos utilizada en invoices.

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,31 @@
+import CustomersTable from "@/app/ui/customers/table";
+import { montserrat } from "@/app/ui/fonts";
+import Search from "@/app/ui/search";
+import { CustomersTableSkeleton } from "@/app/ui/skeletons";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
    title: "Customers",
 };
 
-export default function CustomersPage() {
-    return <p>Customers Page</p>
+export default async function CustomersPage({
+   searchParams,
+}: {
+   searchParams?: Promise<{ query?: string; page?: string }>;
+}) {
+   const resolvedSearchParams = await searchParams;
+   const query = resolvedSearchParams?.query || "";
+
+   return (
+      <div className="w-full">
+         <h1 className={`${montserrat.className} mb-8 text-xl md:text-2xl`}>
+            Customers
+         </h1>
+         <Search placeholder="Search customers..." />
+         <Suspense key={query} fallback={<CustomersTableSkeleton />}>
+            <CustomersTable query={query} />
+         </Suspense>
+      </div>
+   );
 }
